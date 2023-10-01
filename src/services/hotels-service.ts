898a1f13c1369e61e219c03eac9651156b4b93bf
Matheus <matheusvmja@gmail.com>
2023-10-01 18:55:34 -0300

    Fix: hotels service errors

--- src/services/hotels-service.ts
+++ src/services/hotels-service.ts
@@ -1,3 +1,4 @@
+import { notFoundError } from "@/errors";
 import { paymentNotFoundError } from "@/errors/payment-not-found-error";
 import { enrollmentRepository } from "@/repositories";
 import { hotelsRepository } from "@/repositories/hotels-repository";
@@ -6,11 +7,15 @@ async function getHotels(userId: number) {
 
     const ticketEnrollment = await enrollmentRepository.ticketValidation(userId)
 
+    if (!ticketEnrollment) throw notFoundError()
+
     if (ticketEnrollment.Ticket.status !== 'PAID') throw paymentNotFoundError()
 
     if (!ticketEnrollment.Ticket.TicketType.includesHotel || ticketEnrollment.Ticket.TicketType.isRemote) throw paymentNotFoundError()
 
     const hotels = await hotelsRepository.getHotelsList()
+
+    if (hotels.length === 0) throw notFoundError()
     
     return hotels
 }
