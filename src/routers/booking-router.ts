c75ee844041c690467ce522bceae8534421e58be
Matheus <matheusvmja@gmail.com>
2023-10-09 10:43:08 -0300

    Fix: updateBooking router

--- src/routers/booking-router.ts
+++ src/routers/booking-router.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { authenticateToken, validateBody } from '@/middlewares';
-import { getBooking, createBooking } from '@/controllers/booking-controller';
+import { getBooking, createBooking, updateBooking } from '@/controllers/booking-controller';
 import { bookingSchema } from '@/schemas/booking-schema';
 
 const bookingRouter = Router();
@@ -9,6 +9,6 @@ bookingRouter
   .all('/*', authenticateToken)
   .get('/', getBooking)
   .post('/', validateBody(bookingSchema), createBooking)
-  .put('/:bookingId', validateBody(bookingSchema), )
+  .put('/:bookingId', validateBody(bookingSchema), updateBooking)
 
 export { bookingRouter };
